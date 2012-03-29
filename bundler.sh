c5e124dc783d6dcd50f4c3118fd8879bd51046ac
Andrea Franz <andrea@gravityblast.com>
2012-03-29 10:47:05 +0200

    added mate-bundle

--- bundler.sh
+++ bundler.sh
@@ -1,1 +1,6 @@
 alias b='bundle exec'
+
+function mate-bundle() {
+  local location
+  location=`bundle show ${*}` && echo "$(mate "$location" &)" || echo "Could not find gem ${*} in current bundle"
+}
