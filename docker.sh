e30cbfe3c2467c5bb001927dad07504db669de3a
Andrea Franz <andrea@gravityblast.com>
2015-10-28 18:06:48 +0000

    docker aliases

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 alias docker-stop-all="docker ps -q | xargs docker stop"
-alias docker-rm-all="docker ps -a -q | xargs docker rm"
+alias docker-rm-all="docker ps -a -q | xargs docker rm -vf"
 alias docker-kill-all="docker ps -a -q | xargs docker kill"
+alias docker-kr-all="docker-kill-all; docker-rm-all"
 alias docker-sr-all="docker-stop-all; docker-rm-all"
 alias docker-rmi-none="docker images | grep none | awk '{print $3}' | xargs docker rmi"
