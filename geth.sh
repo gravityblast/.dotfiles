0c02e951fafcccd7ada8adec470dd3ed88dba45e
Andrea Franz <andrea@gravityblast.com>
2018-11-08 15:48:19 +0100

    geth syncmode light

--- geth.sh
+++ geth.sh
@@ -1,2 +1,2 @@
-export GETH_ROPSTEN_CMD="geth --datadir ~/.ethereum-data-ropsten --networkid 3 --syncmode fast --verbosity 4 --cache 2048"
+export GETH_ROPSTEN_CMD="geth --datadir ~/.ethereum-data-ropsten --networkid 3 --syncmode light --verbosity 4 --cache 2048"
 alias geth-ropsten="echo $GETH_ROPSTEN_CMD && $GETH_ROPSTEN_CMD"
