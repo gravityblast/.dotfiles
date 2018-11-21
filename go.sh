08be15506a1ece1fb6e2f775e2b5855043547895
Andrea Franz <andrea@gravityblast.com>
2018-11-21 02:33:14 +0100

    update for arch

--- go.sh
+++ go.sh
@@ -2,3 +2,4 @@ export GOPATH=$HOME/dev/go
 PATH=$PATH:$GOPATH/bin
 
 alias cdgo="cd $GOPATH/src/github.com/pilu"
+alias cds="cd $GOPATH/src/github.com/status-im"
--- node.sh
+++ node.sh
@@ -1,7 +1,3 @@
-lazy_source () {
-    eval "$1 () { [ -f $2 ] && source $2 && $1 \$@ }"
-}
-
 export NVM_DIR="$HOME/.nvm"
-lazy_source nvm "/usr/local/opt/nvm/nvm.sh"
-# . "/usr/local/opt/nvm/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
