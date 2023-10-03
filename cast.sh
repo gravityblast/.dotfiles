806ee430746c778783bad73f08e6a833d9771589
Andrea Franz <andrea@gravityblast.com>
2023-10-04 01:42:29 +0200

    add cast and elixir files

---
+++ cast.sh
@@ -0,0 +1,6 @@
+alias cast-call-fantom="cast call --rpc-url https://rpc.fantom.network"
+alias cast-call-mainnet="cast call --rpc-url https://mainnet.infura.io/v3/$INFURA_API_KEY"
+alias cast-call-pgn="cast call --rpc-url https://rpc.publicgoods.network"
+alias cast-call-pgn-testnet="cast call --rpc-url https://sepolia.rpc.publicgoods.network"
+alias cast-call-goerli="cast call --rpc-url https://goerli.infura.io/v3/$INFURA_API_KEY"
+alias cast-call-local="cast call --rpc-url http://127.0.0.1:8545"
